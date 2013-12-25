1377a8566f4960c3f8da5a8b8bfe09afb1dfce24
slicebo123 <slicebo123@gmail.com>
2013-12-25 00:59:05 -0800

    Comment shit out

--- server.js
+++ server.js
@@ -12,16 +12,15 @@ app.use(logfmt.requestLogger());
 //set up server routing
 app.get("/", function(request, response) {
   var content = fs.readFileSync("template.html");
-  getTweets(function(tweets) {
-    var ul = "";
-    tweets.forEach(function(tweet) {
-      ul += "<li><strong>"+tweet.user.screen_name + " => </strong>"+tweet.text+"</li>";
-    });
-
-    content = content.toString('utf8').replace("{{INITIAL_TWEETS}}", ul);
-    response.setHeader("Content-Type", 'text/html')
-    response.send(content);
-  });
+  content = content.toString('utf8').replace("{{INITIAL_TWEETS}}", ul);
+  response.setHeader("Content-Type", 'text/html')
+  response.send(content);
+  // getTweets(function(tweets) {
+  //   var ul = "";
+  //   tweets.forEach(function(tweet) {
+  //     ul += "<li><strong>"+tweet.user.screen_name + " => </strong>"+tweet.text+"</li>";
+  //   });
+  // });
 });
 
 //starts the server
@@ -31,19 +30,19 @@ server.listen(port);
 
 
 //initialize connection to database
-var mongo = require("mongodb");
-var host = '127.0.0.1'
-var port = mongo.Connection.DEFAULT_PORT;
-var db = new mongo.Db("nodejs-introduction", mongo.Server(host, port, {}));
+// var mongo = require("mongodb");
+// var host = '127.0.0.1'
+// var port = mongo.Connection.DEFAULT_PORT;
+// var db = new mongo.Db("nodejs-introduction", mongo.Server(host, port, {}));
 
-var tweetCollection;
+// var tweetCollection;
 
-db.open(function(error) {
-  console.log("You have successfully connected to the database at "+host+":"+port+"!");
-  db.collection('tweet', function(error, collection) {
-    tweetCollection = collection;
-  });
-});
+// db.open(function(error) {
+//   console.log("You have successfully connected to the database at "+host+":"+port+"!");
+//   db.collection('tweet', function(error, collection) {
+//     tweetCollection = collection;
+//   });
+// });
 
 //Render logic to display 10 most recent tweets
 function getTweets (callback) {
@@ -70,13 +69,13 @@ twitter.verifyCredentials(function (err, data) {
 stream('statuses/filter', {track: 'Jon Kitna'}, function(stream) {
   stream.on('data', function(tweet) {
     io.sockets.emit('tweet', tweet);
-    tweetCollection.insert(tweet, function(error) {
-      if (error) {
-        console.log("Oh no an error:", error);
-      } else {
-        console.log("Succesfully inserted tweet");
-      }
-    });
+    // tweetCollection.insert(tweet, function(error) {
+    //   if (error) {
+    //     console.log("Oh no an error:", error);
+    //   } else {
+    //     console.log("Succesfully inserted tweet");
+    //   }
+    // });
   });
 });
 
